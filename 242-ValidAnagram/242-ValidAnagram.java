e8e8b81da9bf6e48957c94fa778f620b4bac9fbc
Sathwik Edupuganti <35898371+SathwikEd@users.noreply.github.com>
2025-03-17 07:07:30 -0500

    File updated at 3/17/2025, 7:07:30 AM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -3,11 +3,15 @@ class Solution {
         if(s.length() == t.length()){
             HashMap<Character, Integer> s_chars = new HashMap<>();
             HashMap<Character, Integer> t_chars = new HashMap<>();
-            for(char c: s.toCharArray()){
-                s_chars.put(c, s_chars.getOrDefault(c,0) +1);
-            }
-            for(char c: t.toCharArray()){
-                t_chars.put(c, t_chars.getOrDefault(c,0) + 1);
+            // for(char c: s.toCharArray()){
+            //     s_chars.put(c, s_chars.getOrDefault(c,0) +1);
+            // }
+            // for(char c: t.toCharArray()){
+            //     t_chars.put(c, t_chars.getOrDefault(c,0) + 1);
+            // }
+            for(int i=0;i<s.length();i++){
+                s_chars.put(s.charAt(i), s_chars.getOrDefault(s.charAt(i), 0) + 1);
+                t_chars.put(t.charAt(i), t_chars.getOrDefault(t.charAt(i), 0) + 1);
             }
             return s_chars.equals(t_chars);
         }
