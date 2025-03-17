d667e043472337d889cf28dddb158fc00348c767
Sathwik Edupuganti <35898371+SathwikEd@users.noreply.github.com>
2025-03-17 06:58:37 -0500

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() == t.length()){
+            HashMap<Character, Integer> s_chars = new HashMap<>();
+            HashMap<Character, Integer> t_chars = new HashMap<>();
+            for(char c: s.toCharArray()){
+                s_chars.put(c, s_chars.getOrDefault(c,0) +1);
+            }
+            for(char c: t.toCharArray()){
+                t_chars.put(c, t_chars.getOrDefault(c,0) + 1);
+            }
+            return s_chars.equals(t_chars);
+        }
+        return false;
+    }
+}
